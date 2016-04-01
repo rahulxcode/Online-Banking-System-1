fed282bc7739b9ead0400e94761f7e52f4cdd2ba
rahulxcode <rahulkhandelwal429@gmail.com>
2016-04-01 11:40:39 +0530

    Create adminhome.php

---
+++ adminhome.php
@@ -0,0 +1,38 @@
+# Online-Banking-System
+Online Banking System in PHP &amp; MySQL
+-------------------------------------------------
+<html>
+<head>
+	<title>Homepage</title>
+	<link href="style.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+
+	<div id="top">
+		<h1 align="center" style="color:white">Admin Home </h1>
+	</div>
+	
+	<div id="header">
+		
+	</div>
+	<h2 align="center">Welcome     Rahul Khandelwal</h2>
+	<h3 align="center">email: rahulkhandelwal429@gmail.com</h3>
+	
+	<div id="leftbox1">	
+		<br><div id="home"><p align="center" style="color:white"><b>Admin Home</b></p></div>
+		
+			<ul>
+				<li><a href="adminhome.php">Home</a></li><br>
+				<li><a href="home.php">User Details</a></li><br>
+				<li>Change password</li><br>
+				<li><a href="loginpage.php">Logout</li></a>
+			</ul>
+	
+	</div>
+	
+	<div id="footer1">
+		<p align="center"><b> &copy Copyright 2015.All rights reserved</b></p>
+	</div>
+	
+</body>
+</html>
