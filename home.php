9c843308af087b20fd167e9fbbbe562697f1e2c2
rahulxcode <rahulkhandelwal429@gmail.com>
2016-04-01 11:54:16 +0530

    Create home.php

---
+++ home.php
@@ -0,0 +1,69 @@
+<html>
+<head>
+	<title>Homepage</title>
+	<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+	<div id="top">
+		<h1 align="center" style="color:white">Welcome !!</h1>
+	</div>
+	
+	<div id="header1">
+		
+	</div>
+	
+	
+	<div id="leftbox">
+		
+		<br><div id="home"><p align="center" style="color:white"><b>Admin Home</div></b></p>
+		
+			<ul>
+				<li><a href="adminhome.php">Home</a></li><br>
+				<li><a href="home.php">User Details</a></li><br>
+				<li>Change password</li><br>
+				<li><a href="loginpage.php">Logout</li></a>
+			</ul>
+	
+	</div>
+	
+	
+	
+	<div id="customer">
+		<div id="customer1">
+			 <h3 align="center" style="color:white"><b>Customers<b></h3>
+			
+		</div>
+	   
+		<h4 align="center">
+			
+			<b><a href="form.php">Add Customers</a></b><br><br>
+			<b><a href="update.php">Edit Customers</a></b><br><br>
+			<b><a href="delete.php">Delete Customers</a></b>
+	   </h4>	
+	
+	</div>
+
+	<div id="staff">
+		
+		<div id="staff1">
+			 <h3 align="center" style="color:white"><b>Staff<b></h3>
+			
+		</div>
+	
+		<h4 align="center">
+			
+			<b><a href="formstaff.php">Add staff</a></b><br><br>
+			<b><a href="update.php">Edit staff</a></b><br><br>
+			<b><a href="delete.php">Delete staff</a></b>
+	   </h4>
+		
+	</div>
+
+	<div id="footer">
+	
+		<p align="center"><b> &copy Copyright 2015.All rights reserved</b></p>
+	</div>
+	
+</body>
+</html>
